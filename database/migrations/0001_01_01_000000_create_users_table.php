9ef07a57a0064903a8363aa54f50898a140b3f44
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-10-13 18:54:23 +0100

    Optional Phone

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('role');
             $table->string('school_id')->nullable();
             $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
